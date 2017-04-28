7062ee3aff9f6671eef63784a7d76a40bbc4fd1d
Yassir KHALDI <khaldi.yass@gmail.com>
2017-04-28 17:33:08 +0100

    Details de l'article, des images de test

---
+++ Details.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="Details du produit" Language="C#" MasterPageFile="~/Layout_members.master" AutoEventWireup="true" CodeFile="Details.aspx.cs" Inherits="Details" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <div class="well" runat="server">
+        <legend>Details sur le produit:</legend>   
+        
+        <div class="row container-fluid" runat="server">
+            <div class="col-md-7">
+
+                <div class="row">
+                    <span class="col-md-4 control-label">Designation: </span>
+                    <asp:Label ID="ldesignation" runat="server" Text="Label"></asp:Label>
+                </div>
+                <hr />
+                <div class="row">
+                    <span class="col-md-4 control-label">Prix unitaire: </span>
+                    <asp:Label ID="lprix" runat="server" Text="Label"></asp:Label>
+                </div>
+                <hr />
+                <div class="row">
+                    <span class="col-md-4 control-label">Stock: </span>
+                    <asp:Label ID="lstock" runat="server" Text="Label"></asp:Label>
+                </div>
+                <hr />
+                <div class="row">
+                    <span class="col-md-4 control-label">Numero de l'article: </span>
+                    <asp:Label ID="lnumArticle" runat="server" Text="Label"></asp:Label>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <asp:Image ID="Image1" runat="server" CssClass="img img-responsive" ImageAlign="Middle" Width="300px" />
+            </div>
+        </div>
+
+        <hr />
+        <div class="row text-center">
+            <asp:HyperLink class="btn btn-primary" ID="HyperLink1" runat="server" NavigateUrl="~/ListCat.aspx">Retour aux articles</asp:HyperLink>
+        </div>
+            
+    </div>
+    
+
+</asp:Content>
+
---
+++ Details.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Details : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["connected"] != null && Session["connected"].ToString().Equals("true"))
+        {
+            MasterPage master = this.Master;
+            (this.Master as Layout_members).username(Session["nom"].ToString(), Session["prenom"].ToString());
+
+            if (Request.QueryString["articleId"] != null)
+            {
+                loadInfos();
+            }
+            else
+            {
+                Response.Redirect("ListCat.aspx");
+            }
+        }
+        else
+        {
+            Response.Redirect("Login.aspx");
+        }
+    }
+
+    protected void loadInfos()
+    {
+        try
+        {
+            string conString = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(conString);
+            conn.Open();
+
+            SqlCommand commande = new SqlCommand("SELECT * FROM article WHERE numArticle = @numArticle", conn);
+            SqlParameter param = new SqlParameter("@numArticle", Request.QueryString["articleId"]);
+            commande.Parameters.Add(param);
+            SqlDataReader reader = commande.ExecuteReader();
+
+            if (reader.Read())
+            {
+                lnumArticle.Text = Convert.ToString(reader[0]) + ".";
+                ldesignation.Text = Convert.ToString(reader[1]) + ".";
+                lprix.Text = Convert.ToString(reader[2]) + " DH.";
+                lstock.Text = Convert.ToString(reader[3]) + " unités.";
+                Image1.ImageUrl = Convert.ToString(reader[4]);
+            }
+            else
+            {
+                Response.Write("Une erreur est survenue lors de l'operation (Le produit demandé n'existe pas)");
+            }
+
+            reader.Close();
+            conn.Close();
+            
+        }
+        catch (Exception exce)
+        {
+            Response.Write(exce.Message);
+        }
+    }
+}
