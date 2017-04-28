01da7b596303622afff932181fcdc8f4c072c151
Yassir KHALDI <khaldi.yass@gmail.com>
2017-04-29 00:04:27 +0100

    Modele de la classe Panier et Commande ajouté

---
+++ App_Code/Commande.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Commande
+/// </summary>
+public class Commande
+{
+    public int NumCommande { get; set; }
+    public string DateCommande { get; set; }
+    public int QteArticle { get; set; }
+
+    public int NumClient { get; set; }
+
+    public int NumArticle { get; set; }
+    public float PrixUnitaire { get; set; }
+    public string DesignationArticle { get; set; }
+    public string PhotoArticle { get; set; }
+
+
+    public Commande()
+    { }
+
+    public float getTotal()
+    {
+        return PrixUnitaire * QteArticle;
+    }
+
+}
---
+++ App_Code/Panier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Panier
+/// </summary>
+public class Panier
+{
+    public int IdPanier { get; set; }
+    public int UserId { get; set; }
+    public List<Commande> ListeCommandes { get; set; }
+
+    public Panier()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public float getTotale()
+    {
+        float somme = 0;
+        foreach(Commande c in ListeCommandes)
+        {
+            somme += c.getTotal();
+        }
+
+        return somme;
+    }
+}
