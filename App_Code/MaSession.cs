e1dd3ceae953def94a45fe868a6f4c56f0a4c36c
Yassir KHALDI <khaldi.yass@gmail.com>
2017-04-29 16:21:26 +0100

    Modeles ajoutes

---
+++ App_Code/MaSession.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for MaSession
+/// </summary>
+public class MaSession
+{
+    public string SessionId { get; set; }
+    public string Login { get; set; }
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
+    public Panier Panier { get; set; }
+    public string Error { get; set; }
+    public string Success { get; set; }
+    
+    public MaSession(string login, string nom, string prenom)
+    {
+        SessionId = Guid.NewGuid().ToString();
+        Login = login;
+        Nom = nom;
+        Prenom = prenom;
+        Panier = new Panier();
+    }
+
+}
